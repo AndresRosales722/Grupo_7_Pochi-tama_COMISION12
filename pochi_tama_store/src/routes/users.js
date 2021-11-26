3e2ddfef43f5dd21142707f9fb467667a38ad89c
Maria <mmariabelenn@gmail.com>
2021-11-26 11:05:14 -0300

    users rooteados

--- pochi_tama_store/src/routes/users.js
+++ pochi_tama_store/src/routes/users.js
@@ -2,8 +2,8 @@ let express = require('express')
 let router = express.Router() 
 let controller = require('../controllers/usersController')
 
-
-
+router.get('/login', controller.login)
+router.get('/register', controller.register)
 
 
 module.exports = router
