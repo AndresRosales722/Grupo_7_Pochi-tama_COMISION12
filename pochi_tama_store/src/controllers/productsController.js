0ec8f48d312617e72c58693302ef2fef91dd7e19
AndresRosales722 <drossandres722@gmail.com>
2021-12-09 21:33:05 -0300

    rama nueva

--- pochi_tama_store/src/controllers/productsController.js
+++ pochi_tama_store/src/controllers/productsController.js
@@ -1,11 +1,82 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../database/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const writeJson = dataBase => fs.writeFileSync(productsFilePath,JSON.stringify(dataBase), 'utf-8')
+
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+
 let controller = {
 
+    index:(req,res)=>{
+        res.render('products/products',{      
+            products,
+            toThousand
+        })
+    },
+    detail:(req,res)=>{
+    let productId = +req.params.id
+    let product = products.find(product => product.id === productId)
+
+        res.render('products/productdetail',{
+            product,
+            toThousand
+        })
+    },
+    
     cart:(req,res)=>{
         res.render('products/productCart')
     },
-    detail:(req,res)=>{
-        res.render('products/productdetail')
-    }
+
+    
+    create:(req,res)=>{
+        res.render('productCreate')
+    },
+
+
+    store:(req,res)=>{
+        const {name,price,discount,category,description} = req.body
+
+		let lastId = 1
+
+		products.forEach(product => {
+			if(product.id > lastId){
+				lastId = product.id
+			}
+		});
+
+		let newProduct ={
+			id : lastId + 1, 
+			name,
+			price,
+			discount,
+			description,
+			category,
+			image: "default-image.png"
+		}
+
+			products.push(newProduct)
+
+			writeJson(products)
+
+			res.redirect('/products')
+    },
+    edit:(req,res)=>{
+        let productId = +req.params.id
+        let productToEdit = products.find(product => product.id === productId)
+
+        res.render('productEdit',{
+            product: productToEdit
+        })
+    },
+
+    update:(req,res)=>{
+        res.render('')
+    },
+
     
 }
 
--- pochi_tama_store/src/routes/products.js
+++ pochi_tama_store/src/routes/products.js
@@ -2,10 +2,18 @@ let express = require('express')
 let router = express.Router() 
 let controller = require('../controllers/productsController')
 
+router.get('/',controller.index)   // Listado de productos
 
-router.get('/productCart',controller.cart)
+router.get('/productdetail/:id/', controller.detail);   // Detalle del producto
 
-router.get('/productdetail',controller.detail)
+router.get('/productCart',controller.cart)  // Carrito de compras
+ 
+router.get('/productCreate/', controller.create);  // Creacion de un producto
+
+router.post('/create', controller.store);  // Formulario de creacion del producto
+
+router.get('/:id/edit', controller.edit); 
+router.put('/:id', controller.update); 
 
 
 
