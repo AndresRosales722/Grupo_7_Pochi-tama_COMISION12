27f0609d81921e2cb38ad2fdbe6eb3631147c0c0
AndresRosales722 <drossandres722@gmail.com>
2022-02-15 15:52:06 -0300

    vista product cart terminada y productos nuevos

--- pochi_tama_store/src/controllers/indexController.js
+++ pochi_tama_store/src/controllers/indexController.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const { users, writeUsersJSON , categories} = require('../database/dataBase')
 
 const db = require('../database/models');
 const sequelize = db.sequelize;
@@ -25,8 +24,6 @@ let controller = {
                 products,
                 toThousand,
                 session: req.session,
-                users,
-                categories
             })
         }) 
         .catch((error)=>console.log(error)) 
