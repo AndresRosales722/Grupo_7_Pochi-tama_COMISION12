d6d8201c4f6424b478503bca6666e71b0a90e2f5
AndresRosales722 <drossandres722@gmail.com>
2022-02-25 12:05:27 -0300

    nuevo modo oscuro basico con sass

--- pochi_tama_store/public/js/darkMode.js
+++ pochi_tama_store/public/js/darkMode.js
@@ -8,17 +8,18 @@ window.addEventListener('load', function () {
 
     let $btnSwitch = qs('#switch')
     let $body = qs('body')
-    let $bgFooter = qs('#bg-footer')
+    /* let $bgFooter = qs('#bg-footer')
     let $card = qs('#card-bg')
     let $header = qs('#header')
-    let $header2 = qs('#navigation-bar-desktop')
+    let $header2 = qs('#navigation-bar-desktop') */
+   
 
 
     $btnSwitch.addEventListener('click', () => {
         $body.classList.toggle('dark')
-        $header.classList.toggle('dark-header')
+        /* $header.classList.toggle('dark-header')
         $header2.classList.toggle('dark-header2')
-        $bgFooter.classList.toggle('dark-footer')
+        $bgFooter.classList.toggle('dark-footer') */
         /* $card.classList.toggle('dark-card') */
 
         if($body.classList.contains('dark')){
@@ -29,16 +30,16 @@ window.addEventListener('load', function () {
 
     })
     
-    if(localStorage.getItem('dark-mode','dark-footer','dark-header','dark-header2') === 'true'){
+    if(localStorage.getItem('dark-mode'/* ,'dark-footer','dark-header','dark-header2' */) === 'true'){
         $body.classList.add('dark')
-        $bgFooter.classList.add('dark-footer')
+        /* $bgFooter.classList.add('dark-footer')
         $header.classList.add('dark-header')
-        $header2.classList.add('dark-header2')
+        $header2.classList.add('dark-header2') */
     } else {
         $body.classList.remove('dark')
-        $bgFooter.classList.remove('dark-footer')
+        /* $bgFooter.classList.remove('dark-footer')
         $header.classList.remove('dark-header')
-        $header2.classList.remove('dark-header2')
+        $header2.classList.remove('dark-header2') */
     }
 
 })
